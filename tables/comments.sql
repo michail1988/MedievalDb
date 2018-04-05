70b8b404da1813bb0f1af06f05d46ec56bf2ac1e
michal <michal.labuda@edps.com.pl>
2018-04-05 16:54:39 +0200

    Poprawki po pierwszym wgraniu bazy danych

--- tables/comments.sql
+++ tables/comments.sql
@@ -5,7 +5,7 @@ create table MED_COMMENTS(
     COMMENT varchar(1000),
     DATE DATETIME NOT NULL,
     STATUS VARCHAR(1) NULL
- )
+ );
 
 ALTER TABLE MED_COMMENTS add column ID INTEGER  NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (ID);
 
@@ -18,13 +18,11 @@ ALTER TABLE MED_COMMENTS add column ID INTEGER  NOT NULL AUTO_INCREMENT, ADD PRI
     c.id as parentid,
     (select concat(u.name, ' ', u.surname) FROM MED_USERS u where u.id = c.fk_user) as parentusername,
     c.status as parentconfirmed,
-
     cc.comment as comment, 
     cc.date as date, 
     cc.id as id,
     (select concat(u.name, ' ', u.surname) FROM MED_USERS u where u.id = cc.fk_user) as username,
     cc.status as confirmed
-
     FROM MED_COMMENTS as c
     left JOIN MED_COMMENTS as cc 
       ON cc.FK_PARENTCOMMENT = c.id and c.status = cc.status
--- tables/newsComments.sql
+++ tables/newsComments.sql
@@ -5,7 +5,7 @@ create table MED_NEWS_COMMENTS(
     COMMENT varchar(1000),
     DATE DATETIME NOT NULL,
     STATUS VARCHAR(1) NULL
- )
+ );
 
 ALTER TABLE MED_NEWS_COMMENTS add column ID INTEGER  NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (ID);
 
@@ -19,17 +19,14 @@ ALTER TABLE MED_NEWS_COMMENTS add column ID INTEGER  NOT NULL AUTO_INCREMENT, AD
     c.id as parentid,
     (select concat(u.name, ' ', u.surname) FROM MED_USERS u where u.id = c.fk_user) as parentusername,
     c.status as parentconfirmed,
-
     cc.comment as comment, 
     cc.date as date, 
     cc.id as id,
     (select concat(u.name, ' ', u.surname) FROM MED_USERS u where u.id = cc.fk_user) as username,
     cc.status as confirmed
-
     FROM MED_NEWS_COMMENTS as c
     left JOIN MED_NEWS_COMMENTS as cc 
       ON cc.FK_PARENTCOMMENT = c.id and c.status = cc.status
   where c.FK_PARENTCOMMENT is null
                and c.status = 'Y' 
-  
 ORDER BY c.DATE ASC, cc.DATE ASC;
